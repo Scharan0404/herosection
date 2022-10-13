eb1c8787828a18a7e2fb9f9af6d37d0692298e6c
Scharan0404 <115443691+Scharan0404@users.noreply.github.com>
2022-10-13 07:26:44 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+function toggleMenu()  {
+    const toggleMenu = document.queryselector('.toggleMenu');
+    const navigation = document.queryselector('.navigation');
+    toggleMenu.classList.toggle('active');
+    navigation.classList.toggle('active');
+    
+}
